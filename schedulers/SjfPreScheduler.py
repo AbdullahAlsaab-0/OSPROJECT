599bd580b0b3f06fe1f13fc52bd69659b6abdab3
Abdullah IA <abdullahibrahimsb@gmail.com>
2025-04-18 13:07:20 +0300

    Revert "Update SjfScheduler.py"
    This reverts commit 7ae1b5ed902f4ebad033e26e48cccf1e8119f197.

--- schedulers/SjfPreScheduler.py
+++ schedulers/SjfPreScheduler.py
@@ -1,63 +1,75 @@
-import copy
-import heapq
-
 from schedulers.Scheduler import Scheduler
+import heapq
+import copy
 
 
 class SjfPreScheduler(Scheduler):
 
-    def run(self):
-        processes_copy = copy.deepcopy(self.processes)
-        ready = []
-        i = 0
-        start_time = 0
-        current_process_id = None
+        def run(self):
+            processes_copy = copy.deepcopy(self.processes)
+            remaining_burst = [p["burst"] for p in processes_copy]
+            completed_count = 0
+            i = 0  # index for tracking which processes have arrived
+            heap = []
+            current_process = None
+            current_index = None
+            start_time = None
+            #ihatemylifewhatdoesthisshitevenmean
+            while completed_count < self.num_processes:
+                # Push all processes that have arrived into the heap
+                while i < self.num_processes and processes_copy[i]["arrival"] <= self.current_time:
+                    heapq.heappush(heap, (remaining_burst[i], i, processes_copy[i]))
+                    i += 1
 
-        # Loop through all processes sorted by arrival time
-        while i < self.num_processes or ready:
-            # If there are processes that have arrived, add them to the ready queue
-            while i < self.num_processes and processes_copy[i]["arrival"] <= self.current_time:
-                heapq.heappush(ready, (processes_copy[i]["burst"], i, processes_copy[i]))
-                i += 1
+                # If there's a running process and a new shorter one arrives
+                if current_process and heap:
+                    if heap[0][0] < remaining_burst[current_index]:
+                        self.timeline.append({
+                            "id": current_process["id"],
+                            "start": start_time,
+                            "finish": self.current_time
+                        })
+                        heapq.heappush(heap, (remaining_burst[current_index], current_index, current_process))
+                        current_process = None
 
-            if ready:
-                burst, id, process = heapq.heappop(ready)
-                process_idx = int(process["id"][1:]) - 1
-
-                # if context switch happened update timeline of the previous process and update current_process_id
-                if current_process_id != process["id"]:
-                    if current_process_id:
-                        self.timeline.append({"id": current_process_id,
-                                              "finish": self.current_time,
-                                              "start": start_time})
-                    current_process_id = process["id"]
+                # If no process is currently running, pick the next shortest one
+                if not current_process and heap:
+                    burst_left, current_index, current_process = heapq.heappop(heap)
                     start_time = self.current_time
+                    process_idx = int(current_process["id"][1:]) - 1
+                    self.get_response_time(process_idx, current_process["arrival"])
 
-                self.get_response_time(process_idx, process["arrival"])
+                # If CPU is idle 
+                if not current_process:
+                    if i < self.num_processes:  # If there are still processes to arrive
+                        next_arrival = processes_copy[i]["arrival"]
+                        self.timeline.append({
+                            "id": "idle",
+                            "start": self.current_time,
+                            "finish": next_arrival
+                        })
+                        self.current_time = next_arrival
+                    else:
+                        # This should not happen in a valid input set
+                        break
+                    continue
 
-                process["burst"] -= 1
+                # Execute one unit of the current process
+                remaining_burst[current_index] -= 1
                 self.current_time += 1
 
+                # If the process finishes
+                if remaining_burst[current_index] == 0:
+                    self.timeline.append({
+                        "id": current_process["id"],
+                        "start": start_time,
+                        "finish": self.current_time
+                    })
+                    process_idx = int(current_process["id"][1:]) - 1
+                    self.turnaround_time[process_idx] = self.current_time - current_process["arrival"]
+                    self.waiting_time[process_idx] = self.turnaround_time[process_idx] - processes_copy[process_idx]["burst"]
+                    self.completed.append(processes_copy[process_idx])
+                    current_process = None
+                    completed_count += 1
 
-                # If the current process has finished, update turnaround times and timeline and add it to the completed list
-                if process["burst"] == 0:
-                    self.turnaround_time[process_idx] = self.current_time - process["arrival"]
-                    self.processes[process_idx]["finished"] = self.current_time
-                    self.completed.append(self.processes[process_idx])
-
-                    self.timeline.append({"id": process["id"],
-                                          "finish": self.current_time,
-                                          "start": start_time})
-
-                    current_process_id = None
-                else:
-                    # If the current process hasn't finished, add it back to the ready queue'
-                    heapq.heappush(ready, (process["burst"], id, processes_copy[process_idx]))
-            else:
-                self.handle_idle_time(i)
-
-        # Calculate waiting times for each process
-        for idx, process in enumerate(self.processes):
-            self.waiting_time[idx] = self.turnaround_time[idx] - self.processes[idx]["burst"]
-
-        self.show_stats("Preemptive-SJF")
+            self.show_stats("Preemptive-SJF")
